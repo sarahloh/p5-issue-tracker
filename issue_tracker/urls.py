8b898f61bb4aeeeb8b0d5e7d612076097f5278f8
Sarah Loh <sarah.loh0708@gmail.com>
2018-10-10 22:10:21 +0100

    add tickets app

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
+from accounts import urls as urls_accounts
+from tickets import urls as urls_tickets
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include(urls_accounts)),
+    path('', include(urls_tickets)),
 ]
---
+++ tickets/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    """
+    Return the index.html file
+    """
+    return render(request, "index.html")
