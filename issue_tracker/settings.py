5308a5a9a21c11250b7131f8b82974f068d2594e
Sarah Loh <sarah.loh0708@gmail.com>
2018-10-12 17:23:59 +0100

    Add heroku to allowed hosts

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['sl-issue-tracker.herokuapp.com']
 
 
 # Application definition
